23dfa4c59224b8af8eb871282473417a4fc428f0
Lora Chisholm <113315723+lchisbyu@users.noreply.github.com>
2022-09-16 19:36:14 -0600

    replacement draft index style and js

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const options = {
+    weekday: "short",
+    day:"numeric",
+    month:"long",
+    year:"numeric,"
+};
+
+const nyear = new Date();
+document.getElementById(
+    "copyyear"
+).textContent = &copy; $(nyear.getFullYear()}| Lora Chisholm | Utah
+
+const time = new Date(document.lastModified);
+
+document.getElementById("copyright').textContent = 'Last updated: $(time.getDate()}';
