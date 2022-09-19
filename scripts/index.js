73435e93a37b4c73f92bc8b10bf13e82f23b8e40
Lora Chisholm <113315723+lchisbyu@users.noreply.github.com>
2022-09-19 15:48:25 -0600

    updated index style and js files

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,1 @@
-const options = {
-    weekday: "short",
-    day:"numeric",
-    month:"long",
-    year:"numeric,"
-};
-
-const nyear = new Date();
-document.getElementById(
-    "copyyear"
-).textContent = &copy; $(nyear.getFullYear()}| Lora Chisholm | Utah
-
-const time = new Date(document.lastModified);
-
-document.getElementById("copyright').textContent = 'Last updated: $(time.getDate()}';
+document.getElementById('timestamp').textContent = 'Last Updated: ' + document.lastModified;
