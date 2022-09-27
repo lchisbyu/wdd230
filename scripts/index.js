3487fe85cb8b51920b0382d348b244546187606f
Lora Chisholm <chi20006@byui.edu>
2022-09-26 18:57:59 -0600

    update alt message and timestamp

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
+
+
 document.getElementById('copyright').textContent = new Date().getFullYear();
+
+const oLastModified = new Date(document.lastModified);
+
+const timestamp = "Last updated: " + oLastModified.getMonth() + "/" + oLastModified.getDay() + "/" + oLastModified.getFullYear() +
+  oLastModified.getHours() + ":" + oLastModified.getMinutes() + ":" + oLastModified.getSeconds() 
+
+  document.getElementById('timestamp').textContent = timestamp
+
 
 
